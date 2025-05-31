24daa572f08077632fccdf4f7ea0fb33965efa44
reenanfs <renan.frsantana@gmail.com>
2025-05-30 23:53:03 -0300

    refactor: require env for region

--- src/shared/database/database.ts
+++ src/shared/database/database.ts
@@ -1,7 +1,9 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
-const AWS_REGION = process.env.AWS_REGION;
+import { requireEnv } from '@utils/require-env';
+
+const AWS_REGION = requireEnv('AWS_REGION');
 
 const dynamoClient = new DynamoDBClient({ region: AWS_REGION });
 export const dynamoDb = DynamoDBDocument.from(dynamoClient);
